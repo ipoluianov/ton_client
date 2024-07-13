8216ffa0cc722aa9aebf347f260c31f0617a15ef
ip <ivan.poluianov@gmail.com>
2024-07-13 15:17:24 +0200

    init

---
+++ main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/ton"
+)
+
+// func to get storage map key
+func getShardID(shard *ton.BlockIDExt) string {
+	return fmt.Sprintf("%d|%d", shard.Workchain, shard.Shard)
+}
+
+func getNotSeenShards(ctx context.Context, api ton.APIClientWrapped, shard *ton.BlockIDExt, shardLastSeqno map[string]uint32) (ret []*ton.BlockIDExt, err error) {
+	if no, ok := shardLastSeqno[getShardID(shard)]; ok && no == shard.SeqNo {
+		return nil, nil
+	}
+
+	b, err := api.GetBlockData(ctx, shard)
+	if err != nil {
+		return nil, fmt.Errorf("get block data: %w", err)
+	}
+
+	parents, err := b.BlockInfo.GetParentBlocks()
+	if err != nil {
+		return nil, fmt.Errorf("get parent blocks (%d:%x:%d): %w", shard.Workchain, uint64(shard.Shard), shard.Shard, err)
+	}
+
+	for _, parent := range parents {
+		ext, err := getNotSeenShards(ctx, api, parent, shardLastSeqno)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, ext...)
+	}
+
+	ret = append(ret, shard)
+	return ret, nil
+}
+
+// FYI: You can find more advanced, optimized and parallelized block scanner in payment network implementation:
+// https://github.com/xssnick/ton-payment-network/blob/master/tonpayments/chain/block-scan.go
+
+func main() {
+	client := liteclient.NewConnectionPool()
+
+	cfg, err := liteclient.GetConfigFromUrl(context.Background(), "https://ton.org/global.config.json")
+	if err != nil {
+		log.Fatalln("get config err: ", err.Error())
+		return
+	}
+
+	// connect to mainnet lite servers
+	err = client.AddConnectionsFromConfig(context.Background(), cfg)
+	if err != nil {
+		log.Fatalln("connection err: ", err.Error())
+		return
+	}
+
+	// initialize ton api lite connection wrapper with full proof checks
+	api := ton.NewAPIClient(client, ton.ProofCheckPolicyFast).WithRetry()
+	api.SetTrustedBlockFromConfig(cfg)
+
+	log.Println("checking proofs since config init block, it may take near a minute...")
+
+	master, err := api.GetMasterchainInfo(context.Background())
+	if err != nil {
+		log.Fatalln("get masterchain info err: ", err.Error())
+		return
+	}
+
+	// TIP: you could save and store last trusted master block (master variable data)
+	// for faster initialization later using api.SetTrustedBlock
+
+	log.Println("master proofs chain successfully verified, all data is now safe and trusted!")
+
+	// bound all requests to single lite server for consistency,
+	// if it will go down, another lite server will be used
+	ctx := api.Client().StickyContext(context.Background())
+
+	// storage for last seen shard seqno
+	shardLastSeqno := map[string]uint32{}
+
+	// getting information about other work-chains and shards of first master block
+	// to init storage of last seen shard seq numbers
+	firstShards, err := api.GetBlockShardsInfo(ctx, master)
+	if err != nil {
+		log.Fatalln("get shards err:", err.Error())
+		return
+	}
+	for _, shard := range firstShards {
+		shardLastSeqno[getShardID(shard)] = shard.SeqNo
+	}
+
+	for {
+		log.Printf("scanning %d master block...\n", master.SeqNo)
+
+		// getting information about other work-chains and shards of master block
+		currentShards, err := api.GetBlockShardsInfo(ctx, master)
+		if err != nil {
+			log.Fatalln("get shards err:", err.Error())
+			return
+		}
+
+		// shards in master block may have holes, e.g. shard seqno 2756461, then 2756463, and no 2756462 in master chain
+		// thus we need to scan a bit back in case of discovering a hole, till last seen, to fill the misses.
+		var newShards []*ton.BlockIDExt
+		for _, shard := range currentShards {
+			notSeen, err := getNotSeenShards(ctx, api, shard, shardLastSeqno)
+			if err != nil {
+				log.Fatalln("get not seen shards err:", err.Error())
+				return
+			}
+			shardLastSeqno[getShardID(shard)] = shard.SeqNo
+			newShards = append(newShards, notSeen...)
+		}
+		newShards = append(newShards, master)
+
+		var txList []*tlb.Transaction
+
+		// for each shard block getting transactions
+		for _, shard := range newShards {
+			log.Printf("scanning block %d of shard %x in workchain %d...", shard.SeqNo, uint64(shard.Shard), shard.Workchain)
+
+			var fetchedIDs []ton.TransactionShortInfo
+			var after *ton.TransactionID3
+			var more = true
+
+			// load all transactions in batches with 100 transactions in each while exists
+			for more {
+				fetchedIDs, more, err = api.WaitForBlock(master.SeqNo).GetBlockTransactionsV2(ctx, shard, 100, after)
+				if err != nil {
+					log.Fatalln("get tx ids err:", err.Error())
+					return
+				}
+
+				if more {
+					// set load offset for next query (pagination)
+					after = fetchedIDs[len(fetchedIDs)-1].ID3()
+				}
+
+				for _, id := range fetchedIDs {
+					// get full transaction by id
+					tx, err := api.GetTransaction(ctx, shard, address.NewAddress(0, byte(shard.Workchain), id.Account), id.LT)
+					if err != nil {
+						log.Fatalln("get tx data err:", err.Error())
+						return
+					}
+					txList = append(txList, tx)
+				}
+			}
+		}
+
+		for i, transaction := range txList {
+			log.Println(i, transaction.String())
+		}
+
+		if len(txList) == 0 {
+			log.Printf("no transactions in %d block\n", master.SeqNo)
+		}
+
+		master, err = api.WaitForBlock(master.SeqNo+1).LookupBlock(ctx, master.Workchain, master.Shard, master.SeqNo+1)
+		if err != nil {
+			log.Fatalln("get masterchain info err: ", err.Error())
+			return
+		}
+	}
+}
